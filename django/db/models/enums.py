0ea2cac59d7d03fb3b7401dd68eaead44630d594
rapsealk <piono623@naver.com>
2025-11-09 02:28:49 +0900

    Change to classmethod

--- django/db/models/enums.py
+++ django/db/models/enums.py
@@ -78,8 +78,8 @@ class TextChoices(Choices, StrEnum):
     def _generate_next_value_(name, start, count, last_values):
         return name
     
-    @staticmethod
-    def from_enum(enum_cls, name=None):
+    @classmethod
+    def from_enum(cls, enum_cls, name=None):
         """Create a TextChoices class from an existing Enum class."""
         attrs = {member.name: member.value for member in enum_cls}
-        return TextChoices(name or enum_cls.__name__, attrs)
+        return cls(name or enum_cls.__name__, attrs)
